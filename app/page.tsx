3f503312cc85e85c280b20323c1efe95252dc6b2
lifesmile-ae <ecommerce@lifesmile.ae>
2023-04-05 11:53:36 +0400

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-        <h1>Hello I am Haider Razza - UAE's Best photographer</h1>
+        <h1>Hello I am Haider Razza - Best photographer in the world</h1>
     </div>
  
   )
